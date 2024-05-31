57919c614c18e426002a6a9777c81910e1d9a4c4
Gugustinette <mercier.augustin@outlook.fr>
2024-05-31 19:10:46 +0200

    Add favicon

--- app.vue
+++ app.vue
@@ -5,6 +5,22 @@
   </div>
 </template>
 
+<script setup lang="ts">
+// Favicon
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/streamers-4-palestinians/favicon.ico' },
+    { rel: 'apple-touch-icon', href: '/streamers-4-palestinians/apple-touch-icon.png' },
+  ],
+})
+// SEO
+useSeoMeta({
+  title: 'Streamers 4 Palestinians',
+  description: 'Streamers 4 Palestinians est une initiative de streamers francophones pour soutenir la population palestinienne à Gaza et en Cisjordanie.',
+  url: 'https://gugustinette.github.io/streamers-4-palestinians/',
+})
+</script>
+
 <style>
 :root {
   --color-primary: #027B3E;
